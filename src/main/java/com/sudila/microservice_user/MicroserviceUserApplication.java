38afbc1a1c251fd00c1d8249671378f4ae5f38c7
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-02 21:50:00 +0530

    chore: comment out CommandLineRunner for user registration tokens

--- src/main/java/com/sudila/microservice_user/MicroserviceUserApplication.java
+++ src/main/java/com/sudila/microservice_user/MicroserviceUserApplication.java
@@ -16,37 +16,37 @@ public class MicroserviceUserApplication {
 		SpringApplication.run(MicroserviceUserApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	) {
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("User")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.register(admin).getAccessToken());
-
-			var manager = RegisterRequest.builder()
-					.firstname("Manager")
-					.lastname("User")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.register(manager).getAccessToken());
-
-			var user = RegisterRequest.builder()
-					.firstname("Regular")
-					.lastname("User")
-					.email("user@mail.com")
-					.password("password")
-					.role(USER)
-					.build();
-			System.out.println("User token: " + service.register(user).getAccessToken());
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service
+//	) {
+//		return args -> {
+//			var admin = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("User")
+//					.email("admin@mail.com")
+//					.password("password")
+//					.role(ADMIN)
+//					.build();
+//			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+//
+//			var manager = RegisterRequest.builder()
+//					.firstname("Manager")
+//					.lastname("User")
+//					.email("manager@mail.com")
+//					.password("password")
+//					.role(MANAGER)
+//					.build();
+//			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+//
+//			var user = RegisterRequest.builder()
+//					.firstname("Regular")
+//					.lastname("User")
+//					.email("user@mail.com")
+//					.password("password")
+//					.role(USER)
+//					.build();
+//			System.out.println("User token: " + service.register(user).getAccessToken());
+//		};
+//	}
 }
